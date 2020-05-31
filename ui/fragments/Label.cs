94cf724c5788400236d4063a3308d5756e6ca152
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-05-31 13:00:37 +0200

    fps counter

---
+++ ui/fragments/Label.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public class Label : Godot.Label
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		new System.Threading.Thread(() =>
+		{
+			while (true)
+			{
+				Text = Engine.GetFramesPerSecond() + " fps";
+			}
+		}).Start();
+	}
+	
+}
