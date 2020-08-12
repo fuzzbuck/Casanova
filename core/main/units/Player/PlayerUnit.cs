6fb30d41edfb8a0dec552deafef2a76298011f8b
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-08-12 13:34:23 +0200

    restructure unit

--- core/main/units/Player/PlayerUnit.cs
+++ core/main/units/Player/PlayerUnit.cs
@@ -1,71 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Godot;
 
 namespace Casanova.core.main.units.Player
 {
 	public class PlayerUnit : Unit
 	{
-		// These variables are declared in units/second
-		public static float acceleration = 1800f;
-		public static float decellaration = 800f;
-		public static float max_speed = 215f;
-		public static float speed = 0f;
-		
-		// Declared in lerp fraction/delta
-		public static float rotation_speed = 6.5f;
-		
-		// Declared as a fraction of 1.0f
-		public static float bounciness = 0.85f;
-		public static float lubrication = 0.7f;
-		
-		public static Vector2 Vel;
-		public Vector2 GetInputAxis()
+		public PlayerUnit ()
+		{
+		}
+		public override Vector2 _GetInputAxis()
 		{
 			var axis = new Vector2();
 			axis.x = Input.GetActionStrength("right") - Input.GetActionStrength("left");
 			axis.y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
 			return axis;
-		}
-
-		public void ApplyFriction(float amt)
-		{
-			if (Vel.Length() > amt)
-				Vel -= Vel.Normalized() * amt;
-			else
-				Vel = Vector2.Zero;
-		}
-
-		public void ApplyMovement(Vector2 amt)
-		{
-			Vel += amt;
-			Vel = Vel.Clamped(max_speed);
-		}
-
-		public override void _PhysicsProcess(float delta)
-		{
-			// makes movement look janky on 60hz+ monitors, will use _Process instead which is bound to FPS
-		}
-
-		public override void _Process(float delta)
-		{
-			var axis = GetInputAxis();
-			if (axis == Vector2.Zero)
-				ApplyFriction(decellaration * delta);
-			else
-			{
-				ApplyMovement(axis * acceleration * delta);
-				Rotation = Mathf.LerpAngle(Rotation, axis.Angle(), rotation_speed * delta);
-			}
-			var collision = MoveAndCollide(Vel * delta);
-			
-			
-			if (collision != null)
-			{
-				//Vel = Vel.Bounce(collision.Normal * bounciness);
-				Vel = Vel.Slide(collision.Normal * lubrication);
-			}
-			
-			
-			speed = Vel.Length();
 		}
 	}
 }
--- core/main/units/Unit.cs
+++ core/main/units/Unit.cs
@@ -4,6 +4,72 @@ namespace Casanova.core.main.units
 {
 	public class Unit : KinematicBody2D
 	{
+		public Unit()
+		{
+		}
+
+		// These variables are declared in units/second
+		float Acceleration = 1800f;
+		float Decellaration = 800f;
+		float MaxSpeed = 215f;
+		protected float Speed = 0f;
+		
+		// Declared in lerp fraction/delta
+		float RotationSpeed = 6.5f;
+		
+		// Declared as a fraction of 1.0f
+		float Bounciness = 0.85f;
+		float Lubrication = 0.7f;
+		
+		Vector2 Vel;
+		
+		public void ApplyFriction(float amt)
+		{
+			if (Vel.Length() > amt)
+				Vel -= Vel.Normalized() * amt;
+			else
+				Vel = Vector2.Zero;
+		}
+
+		public void ApplyMovement(Vector2 amt)
+		{
+			Vel += amt;
+			Vel = Vel.Clamped(MaxSpeed);
+		}
+		
+		// todo: make it use pathfinder or something later
+		public virtual Vector2 _GetInputAxis()
+		{
+			return new Vector2(0, 0);
+		}
+		public void ProcessMovement(float delta)
+		{
+			var axis = _GetInputAxis();
+			if (axis == Vector2.Zero)
+				ApplyFriction(Decellaration * delta);
+			else
+			{
+				ApplyMovement(axis * Acceleration * delta);
+				Rotation = Mathf.LerpAngle(Rotation, axis.Angle(), RotationSpeed * delta);
+			}
+			var collision = MoveAndCollide(Vel * delta);
+			
+			
+			if (collision != null)
+			{
+				//Vel = Vel.Bounce(collision.Normal * bounciness);
+				Vel = Vel.Slide(collision.Normal * Lubrication);
+			}
+			
+			
+			Speed = Vel.Length();
+		}
+
+		public override void _Process(float delta)
+		{
+			ProcessMovement(delta);
+		}
+
 		public override void _Ready()
 		{
 		
