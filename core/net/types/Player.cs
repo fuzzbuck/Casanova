937309576f41f44a1a5d1b6d2e0a8f7918bec903
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-08-15 21:01:25 +0200

    Create Player.cs

---
+++ core/net/types/Player.cs
@@ -0,0 +1,19 @@
+﻿using Casanova.core.main.units.Player;
+using Godot;
+
+namespace Casanova.core.net.types
+{
+    public class Player
+    {
+        public int id;
+        public string username;
+        public PlayerUnit unit;
+
+        public Player(int _id, string _username, PlayerUnit _unit)
+        {
+            id = _id;
+            username = _username;
+            unit = _unit;
+        }
+    }
+}
